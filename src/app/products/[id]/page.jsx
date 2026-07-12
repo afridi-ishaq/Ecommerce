cea92ef158359919ae772943c37467cd4aa919f2
Ishaq Afridi <ishaqafridi7119@gmail.com>
2026-07-12 18:21:49 +0500

    new commit on the way

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -1,4 +1,5 @@
 import AddToCartButton from "@/components/AddToCartButton";
+import Link from "next/link"; // 1. Import Link from next/link
 
 async function getProduct(id) {
   const response = await fetch(
@@ -17,23 +18,29 @@ async function getProduct(id) {
 
 export default async function ProductPage({ params }) {
   const { id } = await params;
-
   const product = await getProduct(id);
 
-  return (
-    <div>
+  return ( 
+    <div className="flex flex-col items-center justify-center p-5 m-5 border-gray-200 border-2 border-solid rounded-lg shadow-md">
+      
+      {/* 2. Changed from <button> to <Link> and onClick to href */}
+      <Link 
+        href="/products" 
+        className="bg-blue-500 text-white px-4 py-2 rounded-md top-10 left-1 absolute mt-2 inline-block"
+      >
+        Back to Products
+      </Link>
+
       <img
         src={product.image}
         alt={product.title}
         width="300"
+        className="mb-5 rounded-lg"
       />
 
       <h1>{product.title}</h1>
-
       <p>{product.description}</p>
-
       <h2>${product.price}</h2>
-
       <p>Stock: {product.stock}</p>
 
       <AddToCartButton product={product} />
